b59a807fb64f85e365acfbf4a02930833cf4494f
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-04-25 18:20:31 +0700

    menambahkan void travers

--- singlelinkedlist/singlelinkedlist.cpp
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -82,3 +82,10 @@ bool listEmpty() {
     else
         return false;
 }
+void traverse() {
+    if (listEmpty()) {
+        cout << "List Kosong" << endl;
+        system("pause");
+        system("cls");
+        return;
+    }
