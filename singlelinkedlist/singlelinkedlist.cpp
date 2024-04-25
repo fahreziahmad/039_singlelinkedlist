fe71f6a980d25e4deb6e120484a7c74b7363661a
fahreziahmad <alfahreziahmadsyahyana@gmail.com>
2024-04-25 18:03:08 +0700

    Add project files.

---
+++ singlelinkedlist/singlelinkedlist.cpp
@@ -0,0 +1,13 @@
+// singlelinkedlist.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int noMhs;
+    string name;
+    Node* next;
+};
+
+
